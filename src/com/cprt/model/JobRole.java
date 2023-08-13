e567f4079a67f0e5725de97467f3f4f8bcecf5f5
diegoluanmartins <diegoluanmartins@gmail.com>
2023-08-12 23:45:50 -0300

    Fixed identation

--- src/com/cprt/model/JobRole.java
+++ src/com/cprt/model/JobRole.java
@@ -42,7 +42,7 @@ public enum JobRole {
         public JobRole getNextRole() {
             return MANAGER;
         }
-    },    
+    },
     MANAGER {
         @Override
         public JobRole getNextRole() {
